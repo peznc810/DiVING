a759206965b18498f25b73a5e18e632da1fe14eb
Inch1109 <inchgum1109@gmail.com>
2024-02-18 01:28:39 +0800

    調整nav間距，增加homenav和home的header

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import Navbar from '@/components/layout/default-layout/navbar'
+import { useEffect } from 'react'
+import Navbar2 from '@/components/layout/default-layout/navbar/index-home'
+import HomeHeader from '@/components/home/header'
 import Footer from '@/components/layout/default-layout/footer'
 
 export default function Index() {
+  // 改變頁面body的顏色
+  useEffect(() => {
+    document.body.style.backgroundColor = '#013c64'
+    document.body.style.position = 'relative'
+  }, [])
+
   return (
     <>
-      <Navbar />
+      <Navbar2 />
+      <HomeHeader />
       <Footer />
-      
     </>
   )
 }
