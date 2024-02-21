2a3146be27cc1bbb107c24eae48e30d32bc47ef1
laughterhao <69628388+laughterhao@users.noreply.github.com>
2024-02-21 13:26:14 +0800

    Update _app.js 解決 react function notation

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -4,7 +4,6 @@ import { useEffect } from 'react'
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
 
-  console.log(getLayout)
   // 導入bootstrap的JS函式庫
   useEffect(() => {
     import('bootstrap/dist/js/bootstrap')
