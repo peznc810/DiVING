e821cf856f8f341cbacdaa495be16320c0305a89
Peznc <peznc.810@gmail.com>
2024-02-22 12:11:22 +0800

    1. global.scss新增調整accordion樣式 2. profile新增右邊樣式

--- client/pages/dashboard/index.js
+++ client/pages/dashboard/index.js
@@ -10,6 +10,8 @@ import { RiFileList2Line } from 'react-icons/ri'
 import { TbMessage } from 'react-icons/tb'
 import { FaRegHeart } from 'react-icons/fa'
 import { LuClipboardList } from 'react-icons/lu'
+import { HiOutlineTicket } from 'react-icons/hi'
+import { TbLogout2 } from 'react-icons/tb'
 
 export default function Dashboard() {
   return (
@@ -28,7 +30,8 @@ export default function Dashboard() {
             >
               <div className="container my-4 py-2">
                 {/* 大頭照 */}
-                <div className={`m-auto mb-3 ${styles.avatar}`}>
+                {/* hover 的時候要可以編輯 */}
+                <div className={`${styles.avatar}`}>
                   <Image
                     src="./images/users/woman.jpg"
                     alt="avatar"
@@ -38,8 +41,11 @@ export default function Dashboard() {
                   />
                 </div>
                 {/* 顯示優惠券張數 */}
-                <div className="show-coupon text-center py-3 border-top border-bottom ">
-                  <div className=" text-secondary">1</div>
+                <div className="text-center py-3 border-top border-bottom ">
+                  {/* 這裡還欠hover的動畫 */}
+                  <Link href="/dashboard/coupons" className=" text-secondary">
+                    1
+                  </Link>
                   <div className="">可用優惠券</div>
                 </div>
                 {/* 列表 */}
@@ -58,8 +64,8 @@ export default function Dashboard() {
                   </li>
                   <li className="">
                     <Link className="text-center" href="/dashboard/coupons">
-                      <LuClipboardList />
-                      優惠券
+                      <HiOutlineTicket />
+                      <span className="me-3">優惠券</span>
                     </Link>
                   </li>
                   <li>
@@ -80,13 +86,133 @@ export default function Dashboard() {
                       我的文章
                     </Link>
                   </li>
+                  <li>
+                    <Link className="text-center" href="/dashboard/logout">
+                      <TbLogout2 />
+                      <span className={styles.logout}>登出</span>
+                    </Link>
+                  </li>
                 </ul>
               </div>
             </div>
             {/* 右邊 */}
             <div
-              className={`col-sm-8 border border-1 border-primary rounded-end ${styles['form-container']}`}
-            ></div>
+              className={`col-sm-8 p-0 rounded-end ${styles['form-container']}`}
+            >
+              <div className="container my-4">
+                <div
+                  class="accordion accordion-flush"
+                  id="accordionFlushExample"
+                >
+                  <div class="accordion-item">
+                    <h2 class="accordion-header">
+                      <button
+                        class="fw-medium accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#flush-collapseOne"
+                        aria-expanded="false"
+                        aria-controls="flush-collapseOne"
+                      >
+                        個人資料
+                      </button>
+                    </h2>
+                    <div
+                      id="flush-collapseOne"
+                      class="accordion-collapse collapse"
+                      data-bs-parent="#accordionFlushExample"
+                    >
+                      <form action="">
+                        <div class="accordion-body">
+                          <div className="row">
+                            <div className="col-6">
+                              <label htmlFor="myName" className="form-label">
+                                姓名
+                              </label>
+                              <input
+                                type="text"
+                                id="myName"
+                                className="form-control"
+                              />
+                            </div>
+                            <div className="col-6">
+                              <label htmlFor="myBirth" className="form-label">
+                                生日
+                              </label>
+                              <input
+                                type="date"
+                                id="myBirth"
+                                className="form-control"
+                              />
+                            </div>
+                            <div className="col-12">
+                              <label htmlFor="myEmail" className="form-label">
+                                電子郵件
+                              </label>
+                              <input
+                                type="email"
+                                id="myEmail"
+                                className="form-control"
+                              />
+                            </div>
+                            <div className="col-12">
+                              <label htmlFor="myTel" className="form-label">
+                                電話號碼
+                              </label>
+                              <input
+                                type="tel"
+                                id="myTel"
+                                className="form-control"
+                              />
+                            </div>
+                            <div className="col-3">
+                              <label htmlFor="postcode" className="form-label">
+                                郵遞區號
+                              </label>
+                              <input
+                                type="text"
+                                id="postcode"
+                                className="form-control"
+                              />
+                            </div>
+                            <div className="col-9">聯絡地址</div>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header">
+                      <button
+                        class="fw-medium accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#flush-collapseTwo"
+                        aria-expanded="false"
+                        aria-controls="flush-collapseTwo"
+                      >
+                        密碼管理
+                      </button>
+                    </h2>
+                    <div
+                      id="flush-collapseTwo"
+                      class="accordion-collapse collapse"
+                      data-bs-parent="#accordionFlushExample"
+                    >
+                      <form action="">
+                        <div class="accordion-body">
+                          Placeholder content for this accordion, which is
+                          intended to demonstrate the
+                          <code>.accordion-flush</code> class. This is the
+                          second accordion body. imagine this being filled with
+                          some actual content.
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
