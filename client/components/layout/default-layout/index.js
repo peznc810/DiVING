dc759a8a6aa474ed7d96e04e01d7ebc482792075
Inch1109 <inchgum1109@gmail.com>
2024-02-22 12:01:25 +0800

    刪掉page裡home資料夾

--- client/components/layout/default-layout/index.js
+++ client/components/layout/default-layout/index.js
@@ -3,7 +3,7 @@ import Footer from './footer'
 
 export default function DefaultLayout({ currentPage, children }) {
   // 判斷路徑如果是/home 改變nav背景色
-  const navBackground = currentPage === '/home' ? 'transparent' : '#013c64'
+  const navBackground = currentPage === '/' ? 'transparent' : '#013c64'
 
   return (
     <>
