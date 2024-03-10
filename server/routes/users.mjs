44319276a0e976fddea055ef6ce7825278267564
Peznc <peznc.810@gmail.com>
2024-03-10 23:33:02 +0800

    會員中心API串接

--- server/routes/users.mjs
+++ server/routes/users.mjs
@@ -2,6 +2,7 @@ import express from 'express'
 import multer from 'multer'
 import db from '../db.mjs'
 import jwt from "jsonwebtoken"
+import checkToken from '../middlewares/checkToken.mjs'
 const router = express.Router()
 
 // 引入.env檔
@@ -13,8 +14,7 @@ const upload = multer()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// checkToken跟logout需要
-const blackList = []
+
 // let user
 
 // 登入
@@ -50,7 +50,7 @@ router.post('/logout', checkToken, (req, res) => {
     token = token.slice(7);
   };
   // 如果沒有在名單中，把目前的登出token放入名單中
-  blackList.push(token)
+  // blackList.push(token)
   // 初始化token
   token = jwt.sign({
     id: '',
@@ -178,57 +178,35 @@ router.post('/google-register', async (req, res) => {
 })
 
 // 讀取profile
-router.get('/:id', checkToken,async (req, res) => {
-  console.log(req.params)
+router.get('/:id', checkToken, async (req, res) => {
   const id = req.params.id
+  const checkId = req.decode.id.toString()
+
+  // 確認授權會員與請求取得的會員資料是否為同一人
+  if(checkId !== id) {
+    return res.json({ status: 'error', message: '會員資料存取失敗' })
+  }
+
   const [[userData]] = await db.execute(
     'SELECT * FROM `users` WHERE `id` = ? ',
     [id]
   )
-  if(userData){
-    res.status(200).json({ status:'success',msg: 'test', userData })
+  if (userData) {
+    res.status(200).json({ status: 'success', msg: 'test', userData })
   } else {
-    res.status(404).json({status:'error',msg:'查無資料'})
+    res.status(404).json({ status: 'error', msg: '查無資料' })
   }
 })
 
-
-// 確認token資料
-function checkToken(req, res, next) {
-  let token = req.get('Authorization')
-  // 是否有token
-  if (token && token.indexOf('Bearer ') === 0) {
-
-    // 如果條件符合，取出token
-    token = token.slice(7)
-    // 檢查是否在已登出的名單中
-    if (blackList.includes(token)) {
-      res.status(401).json({ status: 'error', msg: '登入驗證已失效，請重新登入' })
-      // 結束整個middleware
-      return false
-    }
-    // 前台傳留存的token進來，要確認token是否過期
-    jwt.verify(token, secretKey, (err, decode) => {
-      if (err) {
-        return res.status(401).json({
-          status: 'error',
-          msg: '驗證已失效，請重新登入'
-        })
-      } else {
-        // 把解譯過的資料放入req中讓其他狀態可以共用
-        req.decode = decode
-        // 如果尚未過期就繼續執行
-        next()
-      }
-    })
-  } else {
-    // 沒有token
-    res.status(401).json({
-      status: 'error',
-      msg: '查無此驗證資料'
-    })
-    // return false
+// 更新profile
+router.put('/:id/profile', checkToken, (req, res) => {
+  const id = req.params.id
+  const checkId = req.decode.id.toString()
+  if( checkId !== id){
+    return res.json({ status: 'error', message: '會員資料更新失敗' })
   }
-}
+  res.status(200).json({msg:'success'})
+})
+
 
 export default router
