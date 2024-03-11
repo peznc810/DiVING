3bd74d8743cbd2534146dda30bd23c4cf5211ede
Inch1109 <inchgum1109@gmail.com>
2024-03-11 21:20:17 +0800

    新增loading頁面

--- client/pages/event/index.js
+++ client/pages/event/index.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import EventCarousel from '@/components/event/event-carousel'
 import LatestNews from '@/components/event/latest-news'
 import EventList from '@/components/event/event-list'
+import Loading from '@/components/layout/loading/loading'
 import { useEvent } from '@/hooks/use-eventData'
 
 export default function Event() {
+  const [loading, setLoading] = useState(false)
   const eventList = useEvent()
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
+
   return (
     <>
-      <EventCarousel />
-      <LatestNews eventList={eventList} />
-      <EventList eventList={eventList} />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <EventCarousel />
+          <LatestNews eventList={eventList} />
+          <EventList eventList={eventList} />
+        </>
+      )}
     </>
   )
 }
