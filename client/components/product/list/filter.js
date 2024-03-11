c9b631f4b62bc122c69400f4faf01f93973b9072
Hayley <m8i4m3i16@gmail.com>
2024-03-11 11:23:41 +0800

    0311

--- client/components/product/list/filter.js
+++ client/components/product/list/filter.js
@@ -249,7 +249,6 @@ export default function Filter({ setFilterSettings, clearSettings }) {
             </div>
           </div>
           {/* 清除按鍵 */}
-
           <div className="my-3 col text-center">
             <button
               className="btn btn-primary clear-settings"
