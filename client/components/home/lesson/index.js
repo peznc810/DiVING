3530907f3313bbba217d65f984d7e87b1dc9138f
Inch1109 <inchgum1109@gmail.com>
2024-03-15 13:14:07 +0800

    新增首頁影片，調整排版

--- client/components/home/lesson/index.js
+++ client/components/home/lesson/index.js
@@ -8,30 +8,30 @@ const lessonItem = [
     id: '1',
     name: '自由潛水',
     enName: 'Free Diving',
-    img: '/images/home/lesson-free.jpg',
+    video: '/video/home/freediving.mp4',
     content: '自由潛水自由潛水自由潛水自由潛水自由潛水自由潛水自由潛水',
   },
   {
     id: '2',
     name: '水肺潛水',
     enName: 'Scuba Diving',
-    img: '/images/home/lesson-scuba.jpg',
+    video: '/video/home/scuba-diving.mp4',
     content: '',
   },
-  {
-    id: '3',
-    name: '技術潛水',
-    enName: 'Technical Diving',
-    img: '/images/home/lesson-technical.jpg',
-    content: '',
-  },
-  {
-    id: '4',
-    name: '潛水教練課程',
-    enName: 'Diving Instructor',
-    img: '/images/home/lesson-Instructor.jpg',
-    content: '',
-  },
+  // {
+  //   id: '3',
+  //   name: '技術潛水',
+  //   enName: 'Technical Diving',
+  //   video: '/video/home/technical-diving.mp4',
+  //   content: '',
+  // },
+  // {
+  //   id: '4',
+  //   name: '潛水教練課程',
+  //   enName: 'Diving Instructor',
+  //   video: '/video/home/coachdiving.mp4',
+  //   content: '',
+  // },
 ]
 
 export default function LessonSection() {
@@ -40,7 +40,7 @@ export default function LessonSection() {
       <section className={`${styles.LessonSection}`}>
         <div className={`container-fluid `}>
           <div className={` ${styles.titleBlock}`}>
-            <div className={`ps-4 d-flex align-items-end ${styles.title}`}>
+            <div className={` ps-4 d-flex align-items-end ${styles.title}`}>
               <h3 className="me-3 mb-0 text-light">
                 OUR <br /> LESSON
               </h3>
@@ -52,38 +52,38 @@ export default function LessonSection() {
                 <i className="bi bi-caret-right-fill ms-1"></i>
               </Link>
             </div>
-          </div>
 
-          <div
-            className={`d-flex justify-content-center p-0 ${styles.lessonScroll}`}
-          >
-            {lessonItem.map((v) => {
-              return (
-                <div
-                  key={v.id}
-                  className={`${styles.lessonItem} position-relative `}
-                >
-                  <Link href={'#'} className={`${styles.lessonLink} `}>
-                    <div className={`${styles.info} text-center`}>
-                      <h5>{v.name}</h5>
-                      <p className="m-0">{v.enName}</p>
+            <div className={`d-flex p-0 ${styles.lessonScroll} `}>
+              {lessonItem.map((v) => {
+                return (
+                  <div
+                    key={v.id}
+                    className={`flex-shrink-0 ${styles.lessonItem}`}
+                  >
+                    <div className={`${styles.videoDiv} `}>
+                      <video
+                        // ref={videoRef}
+                        src={v.video}
+                        // autoPlay
+                        muted
+                        loop
+                      ></video>
                     </div>
-                    <div className={`${styles.imgDiv}`}>
-                      <Image
-                        src={v.img}
-                        alt=""
-                        fill
-                        style={{ objectFit: 'cover' }}
-                        priority
-                      ></Image>
+                    <div className={`${styles.play}`}>
+                      <i className="bi bi-caret-right-fill"></i>
                     </div>
-                    <div className={`${styles.divHover}`}>
-                      <p>{v.content}</p>
+                    <div className={` ${styles.info}`}>
+                      <div className={`${styles.enName}`}>
+                        <h5 className="">{v.enName}</h5>
+                      </div>
+                      <div>
+                        <p className="m-0">{v.name}</p>
+                      </div>
                     </div>
-                  </Link>
-                </div>
-              )
-            })}
+                  </div>
+                )
+              })}
+            </div>
           </div>
         </div>
       </section>
--- client/config/nav-menu.js
+++ client/config/nav-menu.js
@@ -14,13 +14,8 @@ export const menuItems = [
     label: '所有商品',
     href: '/product',
     children: [
-      { id: '31', label: 'HeleiWaho', href: '/product' },
-      { id: '32', label: 'OceanMax', href: '/product' },
-      { id: '33', label: 'MYSTIC', href: '/product' },
-      { id: '34', label: 'ADISI', href: '/product' },
-      { id: '35', label: 'AROPEC', href: '/product' },
-      { id: '36', label: 'Princeton Tec', href: '/product' },
-      { id: '37', label: 'Unidive', href: '/product' },
+      { id: '31', label: '所有品牌', href: '/product' },
+      { id: '32', label: '所有種類', href: '/product' },
     ],
   },
   {
