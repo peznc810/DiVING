00eb4eeb20f5adffc88b213bc6ba3d072d6693e0
laughterhao <laughterhao@gmail.com>
2024-03-17 19:51:31 +0800

    新增 取得fav.state 以及修改 fav.state API

--- server/routes/lesson.mjs
+++ server/routes/lesson.mjs
@@ -1,7 +1,7 @@
 import express from 'express'
 import multer from 'multer'
 import db from '../db.mjs'
-import jwt from "jsonwebtoken"
+import jwt from 'jsonwebtoken'
 const router = express.Router()
 
 // 引入.env檔
@@ -13,56 +13,93 @@ const upload = multer()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
 // get list
 router.get('/getlist', function (req, res, next) {
-    (async () => {
-        let [lesson] = await db
-        .execute('SELECT * FROM `lesson`')
-        .catch(() => [undefined])
-        res.json(lesson)
-    })();
-
-});
+  (async () => {
+    let [lesson] = await db
+      .execute('SELECT * FROM `lesson`')
+      .catch(() => [undefined])
+    res.json(lesson)
+  })()
+})
 
 // get id
 router.get('/getlist/:id', function (req, res, next) {
-    (async () => {
-        const lid = req.params.id
-        let [lesson] = await db
-        .execute('SELECT * FROM `lesson` WHERE id = ?', [lid])
-        .catch((err) => {
-            console.error(err)
-            return [undefined]
-        })
-        res.json(lesson)
-    })();
-});
+  (async () => {
+    const lid = req.params.id
+    let [lesson] = await db
+      .execute('SELECT * FROM `lesson` WHERE id = ?', [lid])
+      .catch((err) => {
+        console.error(err)
+        return [undefined]
+      })
+    res.json(lesson)
+  })()
+})
 
 //get star
 
 router.get('/getstar/:id', function (req, res, next) {
+  (async () => {
+    const Sid = req.params.id
+    let [star] = await db
+      .execute(
+        'SELECT lesson.title, star.score ,star.comment FROM lesson INNER JOIN star ON lesson.id = ? AND star.lesson_id = ?',
+        [Sid, Sid],
+      )
+      .catch((err) => {
+        console.error(err)
+        return [undefined]
+      })
+    res.json(star)
+  })()
+})
+
+router.post('/fav/:id', async (req, res) => {
+  const newFav = req.body.fav
+  const id = req.params.id
+  try {
+    // 使用 SQL 語法來更新資料庫中的狀態
+    const result = await db.execute(
+      'UPDATE fav SET state = ? WHERE lesson_id = ?',
+      [newFav, id],
+    )
+    res.json({ state: newFav })
+  } catch (err) {
+    console.error('Error executing query', err.stack)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+  // console.log(newFav)
+})
+
+// get fav
+router.get('/getfav/:id', function (req, res, next) {
     (async () => {
         const Sid = req.params.id
+        const userState = req.query.userState
         let [star] = await db
-        .execute('SELECT lesson.title, star.score FROM lesson INNER JOIN star ON lesson.id = ? AND star.lesson_id = ?', [Sid, Sid])
+        .execute('SELECT fav.state FROM fav WHERE fav.user_id = ? AND fav.lesson_id = ?', [userState, Sid])
         .catch((err) => {
             console.error(err)
             return [undefined]
         })
-        res.json(star)
+        if (star && star.length > 0) {
+            const newFavState = star[0];
+            console.log(newFavState);
+            if (newFavState) {
+                console.log(newFavState.state)
+                res.json(newFavState.state)
+            } else {
+                res.json(null)
+            }
+        } else {
+            res.json(null)
+        }
     })();
 });
 
-
-// 註冊
-
-
 // Google註冊
 
-
-
 // 確認token資料
-
 
 export default router
