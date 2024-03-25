4086e08f7a4ac1cd6d8694a92fcf76bdd40138e0
Peznc <peznc.810@gmail.com>
2024-03-25 12:21:46 +0800

    新增忘記密碼功能

--- client/pages/users/new-password.js
+++ client/pages/users/new-password.js
@@ -1,17 +1,43 @@
 import React from 'react'
 import styles from './styles.module.scss'
+
+import { useRouter } from 'next/router'
+
+// Alert
+import { Toaster } from 'react-hot-toast'
+import { notify } from '@/hooks/use-alert'
 export default function Reset() {
+  const router = useRouter()
+
+  const updatePWD = async (e) => {
+    e.preventDefault()
+    const user = new FormData(e.target)
+    const email = localStorage.getItem('email')
+    user.append('email', email)
+    const url = `http://localhost:3005/api/reset-password`
+    await fetch(url, {
+      method: 'PUT',
+      body: user,
+      credentials: 'include',
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        const { status, msg } = result
+        if (result.status === 'success') {
+          notify(msg, status)
+          router.push('/users/login')
+        }
+      })
+      .catch((err) => console.log(err.msg))
+  }
   return (
     <>
       <main className={`${styles['main-style']}`}>
         <div className="d-flex justify-content-center mt-5">
           <div className={`${styles['card-style']} ${styles['card-layout']}`}>
             {/* chrome會有一個自動填入的input功能，但無法修改其樣式，之後還是選用純色做背景較合適 */}
-            <form action="" method="post" autoComplete="off">
+            <form onSubmit={updatePWD}>
               <h2 className="text-center">建立新密碼</h2>
-              <p className="text-center small">
-                給一些有關新密碼的提示訊息，或須知文字
-              </p>
               <div className={`mb-3 ${styles['input-style']}`}>
                 <input
                   type="password"
@@ -30,16 +56,13 @@ export default function Reset() {
                 />
                 <label htmlFor="repassword">確認密碼</label>
               </div>
-              {/* 警示標語 */}
-              <p className="fw-medium text-center text-danger mb-0 d-none">
-                帳號或密碼錯誤
-              </p>
               {/* END */}
-              <div className={`fs-5 fw-medium ${styles.btn}`}>送出</div>
+              <button className={`fs-5 fw-medium ${styles.btn}`}>送出</button>
             </form>
           </div>
         </div>
       </main>
+      <Toaster />
     </>
   )
 }
